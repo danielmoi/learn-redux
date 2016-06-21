f09a5808177f8f109821280edc6400bc3a9618a0
danielmoi <daniel@ampersandmoi.com>
2016-06-21 21:56:09 +1000

    Create actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,34 @@
+// increment
+// index: which post is going to have its count incremented
+export function increment(index) {
+  return {
+    // name of action that gets dispatched
+    type: 'INCREMENT LIKES',
+
+    // data
+    // 'index' is the same as 'index: index'
+    index
+
+  }
+}
+
+// add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// remove comment
+export function removeComment(postId, index) {
+  // postId is the post from which the comment is to be removed from
+  // index is the index of the comment (in that post) to be removed
+  return {
+    type: 'REMOVE COMMENT',
+    postId,
+    index
+  }
+}
