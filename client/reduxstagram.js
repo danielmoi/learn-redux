a8d483df4d85f05af0a6ab1a6133029083185446
danielmoi <daniel@ampersandmoi.com>
2016-06-21 22:13:55 +1000

    Link our store to app with Provider

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { render } from 'react-dom';
 
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
 import css from './styles/style.styl';
 
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+
+
 const router = (
-  <Router history={ browserHistory }>
-    <Route path="/" component={ Main }>
-      <IndexRoute component={ PhotoGrid }></IndexRoute>
-      <Route path="/view/:postId" component={ Single }></Route>
-    </Route>
-  </Router>
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path="/" component={ Main }>
+        <IndexRoute component={ PhotoGrid }></IndexRoute>
+        <Route path="/view/:postId" component={ Single }></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
