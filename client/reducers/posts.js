dbdbce1e793dbce91e2fe4a7244f7d266d72cc3b
danielmoi <daniel@ampersandmoi.com>
2016-06-23 20:05:53 +1000

    Create Posts reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -2,7 +2,15 @@ function posts(state = [], action) {
   switch(action.type) {
     case 'INCREMENT_LIKES':
       console.log('incrementing likes');
+
+      const i = action.index;
+
       // return updated state
+      return [
+        ...state.slice(0, i), // up to the one we are updating
+        { ...state[i], likes: state[i].likes + 1 },
+        ...state.slice(i + 1) // after the one we are updating
+      ]
       default:
         return state;
 
