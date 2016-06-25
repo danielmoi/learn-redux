7e9d2efed049c432999ef41a7ec81a57a12cd33d
danielmoi <daniel@ampersandmoi.com>
2016-06-25 10:59:31 +1000

    Render comments (user, text, button) in Single view

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  _renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{ comment.user }</strong> { comment.text }
+            <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+
+
+  },
   render() {
     return (
-      <div className="comment">
-        COMMENT COMPONENT
+      <div className="comments">
+        { this.props.postComments.map( this._renderComment ) }
       </div>
     )
   }
