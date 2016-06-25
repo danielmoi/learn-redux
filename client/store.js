0504c75fe1a27ccc86cfc13508f9ded51b746e2a
danielmoi <daniel@ampersandmoi.com>
2016-06-25 12:47:36 +1000

    Integrate store into Redux Dev Tools

--- client/store.js
+++ client/store.js
@@ -15,9 +15,12 @@ const defaultState = {
   comments
 }
 
-const store = createStore(rootReducer, defaultState);
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+const store = createStore(rootReducer, defaultState, enhancers);
 
-export const history = syncHistoryWithStore(browserHistory, store);
+export const history = syncHistoryWithStore(browserHistory, store, enhancers);
 
 if (module.hot) {
   module.hot.accept('./reducers/', () => {
