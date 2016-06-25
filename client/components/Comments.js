41e4bda35fbb87b0cf4651add6ac1af1abeaff72
danielmoi <daniel@ampersandmoi.com>
2016-06-25 11:36:56 +1000

    Write reducer for adding comment

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -13,11 +13,21 @@ const Comments = React.createClass({
 
 
   },
+  _handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  },
   render() {
     return (
       <div className="comments">
         { this.props.postComments.map( this._renderComment ) }
-        <form ref="commentForm" className="comment-form">
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={ this._handleSubmit }>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
           <input type="submit" hidden/>
